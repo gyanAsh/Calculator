c5c01333d12dd91cda02113a718ce482293fa5e5
gyanAsh <gyanash36@protonmail.com>
2021-05-04 03:05:30 +0530

    made a working calculator using flutter

--- calculator_intalf/lib/main.dart
+++ calculator_intalf/lib/main.dart
@@ -1,20 +1,246 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import './widgets/CalButton.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyCal());
 }
 
-class MyCal extends StatelessWidget {
+class MyCal extends StatefulWidget {
+  MyCal({Key key}) : super(key: key);
+  @override
+  MyCalState createState() => MyCalState();
+}
+
+class MyCalState extends State<MyCal> {
+  String history = '';
+  String expression = '';
+
+  void numClick(String text) {
+    setState(() {
+      expression += text;
+    });
+  }
+
+  void allClear(String text) {
+    setState(() {
+      history = '';
+      expression = '';
+    });
+  }
+
+  void clear(String text) {
+    setState(() {
+      expression = '';
+    });
+  }
+
+  void backSpace(String text) {
+    setState(() {
+      expression = expression.substring(0, expression.length - 1);
+    });
+  }
+
+  void evaluate(String text) {
+    Parser p = Parser();
+    Expression exp = p.parse(expression);
+    ContextModel cm = ContextModel();
+    double evel = exp.evaluate(EvaluationType.REAL, cm);
+    setState(() {
+      history = expression;
+      expression = evel.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator',
-      home: Scaffold(
-        backgroundColor: Color(0xFF283637),
-        body: Center(
-          child: CalButton(
-            text: '1',
+      home: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Color(0xFF3C4042), Color(0xFF1E2225)],
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Color(0x00ffffff),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(5),
+                child: Text(
+                  history,
+                  style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                    color: Color(0x99A5ADAF),
+                    fontSize: 30,
+                  )),
+                ),
+              ),
+              Container(
+                // margin: EdgeInsets.all(10),
+                child: Text(
+                  expression,
+                  style: GoogleFonts.rubik(
+                      textStyle: TextStyle(
+                    color: Color(0xFFA5ADAF),
+                    fontSize: 60,
+                  )),
+                ),
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CalButton(
+                    text: 'AC',
+                    fillColor: 0xFFFDB82D,
+                    textColor: 0xFFffffff,
+                    callBack: allClear,
+                  ),
+                  CalButton(
+                    text: 'C',
+                    fillColor: 0xFF4C453B,
+                    textColor: 0xFFBE912C,
+                    callBack: clear,
+                  ),
+                  CalButton(
+                    text: '%',
+                    fillColor: 0xFF4C453B,
+                    textColor: 0xFFBE912C,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '*',
+                    fillColor: 0xFF4A335D,
+                    textColor: 0xFFD485F0,
+                    textSize: 30,
+                    callBack: numClick,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CalButton(
+                    text: '7',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '8',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '9',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '/',
+                    fillColor: 0xFF4A335D,
+                    textColor: 0xFFD485F0,
+                    callBack: numClick,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CalButton(
+                    text: '4',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '5',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '6',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '-',
+                    fillColor: 0xFF4A335D,
+                    textColor: 0xFFD485F0,
+                    textSize: 30,
+                    callBack: numClick,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CalButton(
+                    text: '1',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '2',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '3',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '+',
+                    fillColor: 0xFF4A335D,
+                    textColor: 0xFFD485F0,
+                    callBack: numClick,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CalButton(
+                    text: '.',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '0',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: numClick,
+                  ),
+                  CalButton(
+                    text: '<|',
+                    fillColor: 0xFF35393C,
+                    textColor: 0xFFA7B0AF,
+                    callBack: backSpace,
+                  ),
+                  CalButton(
+                    text: '=',
+                    fillColor: 0xFF8D28DC,
+                    textColor: 0xFFFFFFFF,
+                    callBack: evaluate,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- calculator_intalf/lib/widgets/CalButton.dart
+++ calculator_intalf/lib/widgets/CalButton.dart
@@ -3,32 +3,64 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CalButton extends StatelessWidget {
   final String text;
-  // const BorderRadiusGeometry borderRadius = BorderRadius.circular(50);
+  final int fillColor;
+  final int textColor;
+  final double textSize;
+  final double buttonWidthSize;
+  final double buttonHeightSize;
+  final Function callBack;
 
-  const CalButton({Key key, this.text}) : super(key: key);
+  const CalButton(
+      {Key key,
+      this.callBack,
+      this.textSize = 24,
+      this.buttonWidthSize = 75,
+      this.buttonHeightSize = 70,
+      this.text,
+      this.fillColor,
+      this.textColor})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 65,
-      height: 65,
-      child: TextButton(
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))),
-          backgroundColor:
-              MaterialStateProperty.all<Color>(Colors.lightGreen[200]),
-          // foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+    return Container(
+      margin: EdgeInsets.all(7),
+      // decoration: const BoxDecoration(
+      //   gradient: LinearGradient(
+      //     begin: Alignment.topLeft,
+      //     end: Alignment.bottomRight,
+      //     colors: <Color>[Color(0xFF3C4042), Color(0xFF1E2225)],
+      //   ),
+      //   borderRadius: BorderRadius.only(
+      //     topLeft: Radius.circular(30),
+      //     topRight: Radius.circular(30),
+      //     bottomRight: Radius.circular(30),
+      //     bottomLeft: Radius.circular(30),
+      //   ),
+      // ),
+      child: SizedBox(
+        width: buttonWidthSize,
+        height: buttonHeightSize,
+        child: TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40))),
+            backgroundColor: MaterialStateProperty.all<Color>(Color(fillColor)),
+            // foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          ),
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+                textStyle: TextStyle(
+              color: Color(textColor),
+              fontSize: textSize,
+            )),
+          ),
+          onPressed: () {
+            callBack(text);
+          },
         ),
-        child: Text(
-          text,
-          style: GoogleFonts.rubik(
-              textStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-          )),
-        ),
-        onPressed: () {},
       ),
     );
   }
